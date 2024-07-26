d470ae2cb47566e44fc3daeadfa8f2a038f5fc37
Никита <newacc2103@mail.ru>
2024-07-27 01:30:32 +0500

    CHANGED: -character input toLowerCase -added new cases

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,16 @@ public class Main {
             System.out.println("[н]ачать новую игру или [з]авершить?");
             Scanner sc = new Scanner(System.in);
             char choice = sc.nextLine().charAt(0);
+            choice = Character.toLowerCase(choice);
             if(choice=='н'){
                 System.out.println("Начинаю новую игру...");
                 Game game = new Game();
                 game.start();
-            }else{
+            }else if(choice=='з'){
+                System.out.println("Завершаю программу");
                 break;
+            }else{
+                System.out.println("Неверный ввод");
             }
         }
     }
